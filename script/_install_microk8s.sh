9d0f8249d96945e586ff2a1c646a3f0da4bdb047
Andrea Manzi <manzolo@libero.it>
2023-01-19 22:41:35 +0100

    Improve process

--- script/_install_microk8s.sh
+++ script/_install_microk8s.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 HOST_DIR_NAME=$1
-NET=10.0.0 # internal subnet of virtual machines
-#IP="$(hostname | sed -e 's/[^0-9]*//')"
-IP="$(hostname)"
+#NET=10.0.0 # internal subnet of virtual machines
+#OWN_IP="$(hostname | sed -e 's/k8s-[^0-9]*//')"
+#OWN_IP="$(hostname | sed -e 's/k8s-[^0-9]*//')"
+#OWN_IP="${OWN_IP:-250}"
+#IP="$(hostname)"
 #IF="$(ip -o -4 route show to default | awk '{print $5}')"
 #OUT="$(ip -o -4 route show to default | awk '{print $9}')"
+#echo "IP:"$NET.$OWN_IP
+#echo "IF:"$IF
+#echo "OUT:"$OUT
 # configure static cd  - comment this if assigned automatically
-#printf "network:\n version: 2\n renderer: networkd\n ethernets:\n  $IF:\n    addresses:\n     - $NET.$IP/24" | tee /etc/netplan/90-static.yaml
-#sudo netplan apply
-sudo apt install nfs-common -y
+#sudo tee /etc/netplan/90-static.yaml > /dev/null << EOF
+#network:
+# version: 2
+# renderer: networkd
+# ethernets:
+#    $IF:
+#        addresses: [$NET.$OWN_IP/24] 
+#EOF
+
+sudo netplan apply
+#sudo apt install nfs-common -y
 sudo swapoff -a
 sudo snap install microk8s --classic --stable
 # add user to microk8s sudoers group
@@ -17,9 +30,9 @@ sudo usermod -a -G microk8s ubuntu
 #newgrp microk8s
 sudo microk8s status --wait-ready
 
-(cd ~/.kube && sudo microk8s config > config) & disown
-sudo ufw allow in on cni0 && sudo ufw allow out on cni0
-sudo ufw default allow routed
+#(cd ~/.kube && sudo microk8s config > config) & disown
+#sudo ufw allow in on cni0 && sudo ufw allow out on cni0
+#sudo ufw default allow routed
 sudo snap alias microk8s.helm helm
  # helm repo
 #helm repo add stable https://charts.helm.sh/stable
@@ -28,20 +41,17 @@ sudo snap alias microk8s.helm helm
 # add kubect as alias
 sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.kubectl k
-#echo "IP:"$IP
 
-if test "$IP" = "k8s-main"
+if test "$(hostname)" = "k8s-main"
 then sudo microk8s enable dns dashboard helm
-    sudo rm -rf ${HOST_DIR_NAME}/_join_node.sh
-    #JOINCMD=$(microk8s add-node | sed '/microk8s/p' | head -1)
-    JOINCMD=$(sudo microk8s add-node | sed '/microk8s/p' | sed '6!d')
-    #sudo microk8s config | sed -e "s|server: https://$NET.1:16443|server: https://$OUT:16443|" >/etc/kubeconfig
+    #sudo rm -rf ${HOST_DIR_NAME}/_join_node.sh
+    #sudo sh -c 'sudo microk8s config | sed -e "s|server: https://$OUT:16443|server: https://$NET.1:16443|" > /etc/kubeconfig'
 else 
-    sudo microk8s enable dns dashboard helm
+    sudo microk8s enable dns
     sudo ${HOST_DIR_NAME}/script/_join_node.sh &
     BACK_PID=$!
     while kill -0 $BACK_PID ; do
-        echo "still trying to join..."
+        echo "Still trying to join..."
         sleep 10
     done
 fi
--- script/_join_cluster_helper.sh
+++ script/_join_cluster_helper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 HOST_DIR_NAME=$1
 IP="$(hostname)"
-JOINCMD=$(microk8s add-node | sed '/microk8s/p' | sed '6!d')
+#JOINCMD=$(microk8s add-node | sed '/microk8s/p' | sed '6!d')
+rm -rf ${HOST_DIR_NAME}/_join_node.sh
+JOINCMD=$(sudo microk8s add-node | sed '/microk8s/p' | sed '6!d' | sed -r 's|microk8s join (\b[0-9]{1,3}\.){3}[0-9]{1,3}\b:|microk8s join k8s-main:|')
 echo "${JOINCMD##Join node with: }" > ${HOST_DIR_NAME}/script/_join_node.sh
 chmod a+x ${HOST_DIR_NAME}/script/_join_node.sh

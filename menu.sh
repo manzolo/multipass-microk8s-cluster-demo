a8e4445afcc56596da078b628bcf79c631ff4e81
Andrea Manzi <manzolo@libero.it>
2024-02-11 23:18:59 +0100

    improve rancher menu

--- menu.sh
+++ menu.sh
@@ -3,18 +3,24 @@
 # Function for command selection
 select_command() {
     while true; do
-        OPTION=$(whiptail --title "Cluster Management" --menu "Select a command:" 15 60 9 \
+        OPTION=$(whiptail --title "Kubernetes Cluster Management" --menu "Select a command:" 20 80 13 \
         "Cluster Management:" "" \
-        "Create Cluster" "Create a cluster" \
-        "Start Cluster" "Start a cluster" \
-        "Stop Cluster" "Stop a cluster" \
-        "Add Cluster node" "Add a cluster node" \
-        "Destroy Cluster" "Destroy a cluster" \
+        "Create Cluster" "Create a Kubernetes cluster" \
+        "Start Cluster" "Start the Kubernetes cluster" \
+        "Stop Cluster" "Stop the Kubernetes cluster" \
+        "Add Cluster node" "Add a node to the Kubernetes cluster" \
+        "Remove Cluster node" "Remove a node from the Kubernetes cluster" \
+        "Destroy Cluster" "Destroy the Kubernetes cluster" \
         "Load Balancer Management:" "" \
         "Create Nginx Load Balancer" "Create Nginx load balancer" \
         "Destroy Nginx Load Balancer" "Destroy Nginx load balancer" \
         "Start Nginx Load Balancer" "Start Nginx load balancer" \
         "Stop Nginx Load Balancer" "Stop Nginx load balancer" \
+        "Rancher Management:" "" \
+        "Create Rancher" "Create Rancher" \
+        "Destroy Rancher" "Destroy Rancher" \
+        "Start Rancher" "Start Rancher" \
+        "Stop Rancher" "Stop Rancher" \
         "Exit" "Exit the program" 3>&1 1>&2 2>&3)
 
         if [ $? -eq 0 ]; then
@@ -34,6 +40,14 @@ select_command() {
                 "Add Cluster node")
                     ./cmd/add_node.sh || echo "Error during execution of add_node.sh"
                     ;;
+                "Remove Cluster node")
+                    NODE_NAME=$(whiptail --inputbox "Enter the instance name of the node to remove (e.g., k8s-node3):" 8 50 --title "Node Name" 3>&1 1>&2 2>&3)
+                    if [ $? -eq 0 ]; then
+                        ./cmd/remove_node.sh "$NODE_NAME" || echo "Error during execution of remove_node.sh"
+                    else
+                        echo "Exiting..."
+                    fi
+                    ;;
                 "Create Nginx Load Balancer")
                     ./cmd/create_nginx_lb.sh || echo "Error during execution of create_nginx_lb.sh"
                     ;;
@@ -45,6 +59,18 @@ select_command() {
                     ;;
                 "Stop Nginx Load Balancer")
                     multipass stop nginx-cluster-balancer || echo "Error stopping Nginx load balancer"
+                    ;;
+                "Create Rancher")
+                    ./cmd/create_rancher.sh || echo "Error during execution of create_rancher.sh"
+                    ;;
+                "Destroy Rancher")
+                    ./cmd/destroy_rancher.sh || echo "Error during execution of destroy_rancher.sh"
+                    ;;
+                "Start Rancher")
+                    multipass start rancher || echo "Error starting rancher"
+                    ;;
+                "Stop Rancher")
+                    multipass stop rancher || echo "Error stopping rancher"
                     ;;
                 "Exit")
                     echo "Exiting..."
